1164c0860c5b90a222f25e9804439f71addafad7
Maks <maks.pronevich@gmail.com>
2021-03-10 17:11:27 +0300

    modify files

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,7 +1,55 @@
-const catalogBtn = document.querySelector('.btn--catalog');
-const catalog = document.querySelector('.catalog-nav');
+'use strict';
+document.addEventListener("DOMContentLoaded", () => {
 
-catalogBtn.addEventListener("click", e => {
-    catalog.classList.toggle('active');
-    e.preventDefault();
+
+    // functions    
+    const toggleCatalog = () => {
+        const catalogBtnElem = document.querySelector('.btn--catalog');
+        const catalogElem = document.querySelector('.catalog-nav');
+
+        catalogBtnElem.addEventListener("click", e => {
+            catalogElem.classList.toggle('active');
+            e.preventDefault();
+        });
+    };
+
+
+    const catalogDropdown = () => {
+        const catalogNavLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
+        const catalogSubNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
+
+        const deactivate = () => {
+            catalogSubNavListElems.forEach(catalogSubNavListElem => catalogSubNavListElem.classList.remove('active'));
+        };
+
+        catalogNavLinkElems.forEach((catalogNavLinkElem, i) => {
+            catalogNavLinkElem.addEventListener("mouseover", () => {
+                deactivate();
+                catalogSubNavListElems[i].classList.add('active');
+            });
+        });
+
+
+        document.addEventListener('mouseover', e => {
+            if (
+                !(e.target === profileBlock || profileBlock.contains(e.target)) &&
+                !(e.target === profileLink || profileLink.contains(e.target)) &&
+                profileBlock.classList.contains('active')
+            ) {
+                toggleProfileBlock();
+            }
+        });
+    };
+
+
+
+
+
+
+
+    // functions call
+    toggleCatalog();
+    catalogDropdown();
 });
+
+
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,8 +1,40 @@
-"use strict";
+'use strict';
 
-var catalogBtn = document.querySelector('.btn--catalog');
-var catalog = document.querySelector('.catalog-nav');
-catalogBtn.addEventListener("click", function (e) {
-  catalog.classList.toggle('active');
-  e.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  // functions    
+  var toggleCatalog = function toggleCatalog() {
+    var catalogBtnElem = document.querySelector('.btn--catalog');
+    var catalogElem = document.querySelector('.catalog-nav');
+    catalogBtnElem.addEventListener("click", function (e) {
+      catalogElem.classList.toggle('active');
+      e.preventDefault();
+    });
+  };
+
+  var catalogDropdown = function catalogDropdown() {
+    var catalogNavLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
+    var catalogSubNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
+
+    var deactivate = function deactivate() {
+      catalogSubNavListElems.forEach(function (catalogSubNavListElem) {
+        return catalogSubNavListElem.classList.remove('active');
+      });
+    };
+
+    catalogNavLinkElems.forEach(function (catalogNavLinkElem, i) {
+      catalogNavLinkElem.addEventListener("mouseover", function () {
+        deactivate();
+        catalogSubNavListElems[i].classList.add('active');
+      });
+    });
+    document.addEventListener('mouseover', function (e) {
+      if (!(e.target === profileBlock || profileBlock.contains(e.target)) && !(e.target === profileLink || profileLink.contains(e.target)) && profileBlock.classList.contains('active')) {
+        toggleProfileBlock();
+      }
+    });
+  }; // functions call
+
+
+  toggleCatalog();
+  catalogDropdown();
 });
