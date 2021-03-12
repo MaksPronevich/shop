fe0d6ed992a6b84fe6ba7c21897ec3bf17a7520e
Maks <maks.pronevich@gmail.com>
2021-03-12 15:15:36 +0300

    modify files

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,72 +1,56 @@
 'use strict';
-document.addEventListener("DOMContentLoaded", () => {
+// ================================================= functions ==================================================    
+const toggleCatalog = () => {
+    const catalogBtnElem = document.querySelector('.btn-catalog');
+    const catalogElem = document.querySelector('.catalog-wrap');
 
-    // ================================================= functions ==================================================    
-    const toggleCatalog = () => {
-        const catalogBtnElem = document.querySelector('.btn-catalog');
-        const catalogElem = document.querySelector('.catalog-wrap');
+    catalogBtnElem.addEventListener("click", e => {
+        catalogElem.classList.toggle('active');
+        e.preventDefault();
+    });
+};
 
-        catalogBtnElem.addEventListener("click", e => {
-            catalogElem.classList.toggle('active');
-            e.preventDefault();
-        });
+
+const catalogSubNavigation = () => {
+    const navLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
+    const subNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
+
+    const deactivateSubNav = () => {
+        subNavListElems.forEach(subNavListElem => subNavListElem.classList.remove('active'));
     };
 
-
-    const catalogSubNavigation = () => {
-        const navLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
-        const subNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
-        const container = document.querySelector('.catalog-nav');
-
-        const deactivateSubNav = () => {
-            subNavListElems.forEach(subNavListElem => subNavListElem.classList.remove('active'));
-        };
-
-
-        navLinkElems.forEach((navLinkElem, i) => {
-            navLinkElem.addEventListener("mouseover", () => {
-                deactivateSubNav();
-                subNavListElems[i].classList.add('active');
-            });
-            navLinkElem.addEventListener("click", e => e.preventDefault());
+    navLinkElems.forEach((navLinkElem, i) => {
+        navLinkElem.addEventListener("mouseover", () => {
+            deactivateSubNav();
+            subNavListElems[i].classList.add('active');
         });
+        navLinkElem.addEventListener("click", e => e.preventDefault());
+    });
+
+    document.addEventListener('mouseover', e => {
+        if (e.target.closest('.catalog-nav')) return;
+        deactivateSubNav();
+    });
+};
 
 
-        // document.addEventListener('mouseover', e => {
-        //     const target = e.target;
-        //     if (
-        //         // !target.classList.contains('catalog-nav') &&
-        //         // !target.classList.contains('catalog-nav-nav-list') &&
-        //         // !target.classList.contains('catalog-nav-list__item') &&
-        //         // !target.classList.contains('catalog-nav-list__item-link') &&
-
-        //         // !target.classList.contains('catalog-sub-nav') &&
-        //         // !target.classList.contains('catalog-sub-nav-list') &&
-        //         // !target.classList.contains('catalog-sub-nav-list__item') &&
-        //         // !target.classList.contains('catalog-sub-nav-list__item-link') &&
-        //         // !target.classList.contains('catalog-sub-nav-list__item-title')
-        //     ) {
-        //         deactivateSubNav();
-        //     }
-        // });
-        document.addEventListener('mouseover', e => {
-            const target = e.target;
-            if (container.contains(target)) {
-                deactivateSubNav();
-                e.stopPropagation();
-            }
-        });
-
-
-
-    };
+const slider = () => {
+    new Swiper('.swiper-container', {
+        loop: true,
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+    });
+}
 
 
 
 
-    // ================================================= functions call ================================================== 
-    toggleCatalog();
-    catalogSubNavigation();
-});
+// ================================================= functions call ================================================== 
+toggleCatalog();
+catalogSubNavigation();
+slider();
+
 
 
--- dist/js/main.js
+++ dist/js/main.js
@@ -1,63 +1,50 @@
-'use strict';
+'use strict'; // ================================================= functions ==================================================    
 
-document.addEventListener("DOMContentLoaded", function () {
-  // ================================================= functions ==================================================    
-  var toggleCatalog = function toggleCatalog() {
-    var catalogBtnElem = document.querySelector('.btn-catalog');
-    var catalogElem = document.querySelector('.catalog-wrap');
-    catalogBtnElem.addEventListener("click", function (e) {
-      catalogElem.classList.toggle('active');
-      e.preventDefault();
+var toggleCatalog = function toggleCatalog() {
+  var catalogBtnElem = document.querySelector('.btn-catalog');
+  var catalogElem = document.querySelector('.catalog-wrap');
+  catalogBtnElem.addEventListener("click", function (e) {
+    catalogElem.classList.toggle('active');
+    e.preventDefault();
+  });
+};
+
+var catalogSubNavigation = function catalogSubNavigation() {
+  var navLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
+  var subNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
+
+  var deactivateSubNav = function deactivateSubNav() {
+    subNavListElems.forEach(function (subNavListElem) {
+      return subNavListElem.classList.remove('active');
     });
   };
 
-  var catalogSubNavigation = function catalogSubNavigation() {
-    var navLinkElems = document.querySelectorAll('.catalog-nav-list__item-link');
-    var subNavListElems = document.querySelectorAll('.catalog-sub-nav-list');
-    var container = document.querySelector('.catalog-nav');
-
-    var deactivateSubNav = function deactivateSubNav() {
-      subNavListElems.forEach(function (subNavListElem) {
-        return subNavListElem.classList.remove('active');
-      });
-    };
-
-    navLinkElems.forEach(function (navLinkElem, i) {
-      navLinkElem.addEventListener("mouseover", function () {
-        deactivateSubNav();
-        subNavListElems[i].classList.add('active');
-      });
-      navLinkElem.addEventListener("click", function (e) {
-        return e.preventDefault();
-      });
-    }); // document.addEventListener('mouseover', e => {
-    //     const target = e.target;
-    //     if (
-    //         // !target.classList.contains('catalog-nav') &&
-    //         // !target.classList.contains('catalog-nav-nav-list') &&
-    //         // !target.classList.contains('catalog-nav-list__item') &&
-    //         // !target.classList.contains('catalog-nav-list__item-link') &&
-    //         // !target.classList.contains('catalog-sub-nav') &&
-    //         // !target.classList.contains('catalog-sub-nav-list') &&
-    //         // !target.classList.contains('catalog-sub-nav-list__item') &&
-    //         // !target.classList.contains('catalog-sub-nav-list__item-link') &&
-    //         // !target.classList.contains('catalog-sub-nav-list__item-title')
-    //     ) {
-    //         deactivateSubNav();
-    //     }
-    // });
-
-    document.addEventListener('mouseover', function (e) {
-      var target = e.target;
-
-      if (container.contains(target)) {
-        deactivateSubNav();
-        e.stopPropagation();
-      }
+  navLinkElems.forEach(function (navLinkElem, i) {
+    navLinkElem.addEventListener("mouseover", function () {
+      deactivateSubNav();
+      subNavListElems[i].classList.add('active');
     });
-  }; // ================================================= functions call ================================================== 
+    navLinkElem.addEventListener("click", function (e) {
+      return e.preventDefault();
+    });
+  });
+  document.addEventListener('mouseover', function (e) {
+    if (e.target.closest('.catalog-nav')) return;
+    deactivateSubNav();
+  });
+};
+
+var slider = function slider() {
+  new Swiper('.swiper-container', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    }
+  });
+}; // ================================================= functions call ================================================== 
 
 
-  toggleCatalog();
-  catalogSubNavigation();
-});
+toggleCatalog();
+catalogSubNavigation();
+slider();
